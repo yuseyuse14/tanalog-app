3afef7d810bae28c6a384ee23624491cf6c57a1f
yuse <yu.ayamomik@gmail.com>
2026-04-28 23:00:44 +0900

    add: gitignore追加

---
+++ StockManager/StockManagerApp.swift
@@ -0,0 +1,15 @@
+//
+//  StockManagerApp.swift
+//  StockManager
+//
+
+import SwiftUI
+
+@main
+struct StockManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
