779c85c03eaeb0f5fb4185575803e1d180b3bcfa
yuse <yu.ayamomik@gmail.com>
2026-05-02 09:08:18 +0900

    feat: メインのページ遷移ををタブビューで実装

--- StockManager/ContentView.swift
+++ StockManager/ContentView.swift
@@ -5,15 +5,36 @@
 
 import SwiftUI
 
+enum TabItem: Hashable {
+    case home
+    case stocks
+    case dishes
+    case orders
+    case shops
+}
+
 struct ContentView: View {
+    @State private var selectedTab: TabItem? = .home
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView(selection: $selectedTab) {
+            Tab("ホーム", systemImage: "house", value: TabItem.home) {
+                Text("ホーム")
+            }
+            Tab("在庫", systemImage: "shippingbox", value: TabItem.stocks) {
+                Text("在庫一覧")
+            }
+            Tab("メニュー", systemImage: "fork.knife", value: TabItem.dishes) {
+                Text("メニュー一覧")
+            }
+            Tab("発注履歴", systemImage: "cart", value: TabItem.orders) {
+                Text("発注履歴")
+            }
+            Tab("仕入れ先", systemImage: "building.2", value: TabItem.shops) {
+                Text("仕入れ先一覧")
+            }
         }
-        .padding()
+        .tabViewStyle(.sidebarAdaptable)
     }
 }
 
