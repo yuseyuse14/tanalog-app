3c20776941c4c1558a154c6ac891d3cce45dd184
yuse <yu.ayamomik@gmail.com>
2026-05-09 10:38:40 +0900

    feat: 新規追加ボタンを押すと在庫の新規追加画面に繊維する機能を実装

---
+++ StockManager/Views/Stock/StockCreateView.swift
@@ -0,0 +1,16 @@
+//
+//  StockCreateView.swift
+//  StockManager
+//
+
+import SwiftUI
+
+struct StockCreateView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StockCreateView()
+}
