509ff5958bc747b4bed9108e09d8b3fbea32898b
yuse <yu.ayamomik@gmail.com>
2026-05-24 15:30:24 +0900

    feat: ソートメニューで昇順、降順を切り替えられるように変更

--- StockManager/Models/StockFilter.swift
+++ StockManager/Models/StockFilter.swift
@@ -7,20 +7,28 @@ import Foundation
 
 struct StockQuery {
     var sortType: SortType = .name
+    var sortOrder: SortOrder = .forward
 
     mutating func selectSortType(_ newType: SortType) {
         if sortType == newType {
+            sortOrder = (sortOrder == .forward) ? .reverse : .forward
         } else {
             sortType = newType
+            sortOrder = .forward
         }
     }
 
     func sort(stocks: [Stock]) -> [Stock] {
-        return switch sortType {
+        let sortedStock = switch sortType {
         case .name:
             stocks.sorted { $0.name < $1.name }
         case .num:
             stocks.sorted { $0.num < $1.num }
+        }
+
+        return switch sortOrder {
+        case .forward: sortedStock
+        case .reverse: sortedStock.reversed()
         }
     }
 }
