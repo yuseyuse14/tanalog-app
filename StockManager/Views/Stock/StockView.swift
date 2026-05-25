6a249444a53be8d7a6a4cd7f39d5f749892fc199
yuse <yu.ayamomik@gmail.com>
2026-05-26 03:16:22 +0900

    refactor: 在庫一覧の各行をStockListViewに移動

--- StockManager/Views/Stock/StockView.swift
+++ StockManager/Views/Stock/StockView.swift
@@ -134,56 +134,7 @@ struct StockView: View {
                     ScrollView {
                         LazyVStack(spacing: 0) {
                             ForEach(queryStocks) { stock in
-                                VStack(spacing: 0) {
-                                    HStack(spacing: 0) {
-                                        // 在庫状態
-                                        RoundedRectangle(cornerRadius: 4)
-                                            .fill(stock.status.color)
-                                            .frame(width: 8, height: 30)
-                                        // 在庫名
-                                        Text(stock.name)
-                                            .font(.title)
-                                            .frame(maxWidth: .infinity)
-                                        // マイナス
-                                        Button {
-                                            stock.decrement()
-                                        } label: {
-                                            Image(systemName: "minus.square")
-                                                .resizable()
-                                                .frame(width: 24, height: 24)
-                                        }
-                                        // 個数
-                                        Text("\(stock.num)")
-                                            .font(.title)
-                                            .monospacedDigit()
-                                            .frame(width: 55)
-                                        // プラス
-                                        Button {
-                                            stock.increment()
-                                        } label: {
-                                            Image(systemName: "plus.square")
-                                                .resizable()
-                                                .frame(width: 24, height: 24)
-                                        }
-                                    }
-                                    .padding(.vertical, 8)
-                                    .padding(.horizontal, 16)
-                                    .background(
-                                        RoundedRectangle(cornerRadius: 20)
-                                            .fill(selectedStock == stock ? Color(.systemGray).opacity(0.28) : Color.clear)
-                                    )
-                                    .contentShape(Rectangle())
-                                    .onTapGesture {
-                                        withAnimation(.linear(duration: 0.16)) {
-                                            selectedStock = stock
-                                        }
-                                    }
-
-                                    if selectedStock != stock {
-                                        Divider()
-                                            .padding(.horizontal, 28)
-                                    }
-                                }
+                                StockLowView(stock: stock, selectedStock: $selectedStock)
                             }
                         }
                     }
